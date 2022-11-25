06b6c1d6e050c99fb767c99bc62e4f5892e5e8c4
thejohnjohn <jp7480566@gmail.com>
2022-11-25 08:54:22 -0300

    chore: add POST

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const http = require('http');
 
+const { ToolsService } = require('./src/tools/tools.service');
+
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((request, response) => {
-  const { url } = request;
-  console.log(url);
+const toolsService = new ToolsService();
+
+const server = http.createServer(async (request, response) => {
+  const { url, method } = request;
   const regex = /^(\/tools)((\/[0-9]+)|(\?[a-z]+\=[a-z]+((\&[a-z]+\=[a-z]+)+)?))?/;
   const result = regex.exec(url);
   
@@ -13,11 +16,34 @@ const server = http.createServer((request, response) => {
     response.writeHead(404);
     response.end(`The ${url} address is not found.`);
   }
-
-  response.writeHead(200);
-  response.end(`${result}`);
+  
+  switch (method) {
+    case 'GET':
+      let result = await toolsService.getAllTools();
+     
+      response.writeHead(200, { "Content-Type": "application/json" });
+      response.write(JSON.stringify(result));
+      response.end();
+    break;
+    case 'POST':
+      let body = '';
+      request.on('data', chunk => {
+          body += chunk.toString(); // convert Buffer to string
+      });
+      request.on('end', () => {
+          console.log(body);
+          response.writeHead(200, { "Content-Type": "application/json" });
+          response.end(body);
+      });
+    break;
+    default:
+      response.writeHead(404);
+      response.end(`Not found`);
+    break;
+  }
 });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
