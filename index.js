ca8a931213fe0e634b2031e712faa500554f75f2
thejohnjohn <jp7480566@gmail.com>
2022-12-11 11:01:03 -0300

    add express-like route handler

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-const http = require('http');
-const querystring = require('querystring');
-
 const xpress = require('./utils/xpress');
 const { ToolsService } = require('./src/tools/tools.service');
 const { getRequestData } = require('./utils/utils');
@@ -13,26 +10,41 @@ const port = PORT || 3000;
 const toolsService = new ToolsService();
 
 xpress.post('/login', async (request, response) => {
-    let data = await getRequestData(request);
-      
-    data = JSON.parse(data);
+  const data = request.body;
 
-    /* let authenticated = isAuthenticated(data);
+  const authenticated = isAuthenticated(data);
 
-    if (authenticated === -1) {
-      response.writeHead(401);
-      response.write('Unauthorized');
-    } else {
-      const access_token = createToken(data);
+  if (authenticated === -1) {
+    response.status(401).send('Unauthorized');
+  } else {
+    // eslint-disable-next-line camelcase
+    const access_token = createToken(data);
 
-      response.writeHead(200, { "Content-Type": "application/json" });
-      response.write(JSON.stringify({ access_token }));
-    } */
+    // eslint-disable-next-line camelcase
+    response.status(200).send(JSON.stringify({ access_token }));
+  }
+});
 
-    response.writeHead(200);
-    response.write('ok');
-    response.end();
+xpress.put('/tools/4', async (request, response) => {
+  const { url } = request;
+  const id = parseInt(url.split('/')[2], 10);
+
+  const headers = request.rawHeaders;
+  const authorizationIndex = headers.indexOf('Authorization') + 1;
+  const token = headers[authorizationIndex].split('Bearer ')[1];
+  const result = verifyToken(token);
+
+  if (result instanceof Error) {
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+    response.end('Access expired.');
+  }
+
+  const updatedTool = request.body;
+  console.log(updatedTool);
+  /* await toolsService.updateToolById(id, JSON.parse(updatedTool));
+
+  response.writeHead(200, { 'Content-Type': 'application/json' }); */
+  response.status(200).send(JSON.stringify(updatedTool));
 });
 
 xpress.listen(port, () => console.log(`Server running at http://${hostname}:${port}/`));
-
--- utils/xpress.js
+++ utils/xpress.js
@@ -1,80 +1,88 @@
+/* eslint-disable prefer-rest-params */
 const http = require('http');
 
 class Xpress {
-    constructor() {
-        this.server = http.createServer();
-        this.setupRoutes();
-        this.setupMethods();
-        this.server.on('request', this.handleRequest.bind(this));
+  constructor() {
+    this.server = http.createServer();
+    this.setupRoutes();
+    this.setupMethods();
+    this.server.on('request', this.handleRequest.bind(this));
+  }
+
+  setupRoutes() {
+    this.router = {
+      GET: {},
+      POST: {},
+      PUT: {},
+      DEL: {},
+    };
+  }
+
+  setupMethods() {
+    this.get = this.registerRoute('GET');
+    this.post = this.registerRoute('POST');
+    this.put = this.registerRoute('PUT');
+    this.del = this.registerRoute('DEL');
+  }
+
+  // eslint-disable-next-line consistent-return
+  async handleRequest(req, res) {
+    // eslint-disable-next-line no-param-reassign
+    req = await this.setupRequest(req);
+    // eslint-disable-next-line no-param-reassign
+    res = this.setupResponse(res);
+
+    if (!this.router[req.method][req.url]) {
+      res.statusCode = 404;
+      res.write('not found');
+      return res.end();
     }
 
-    setupRoutes() {
-        this.router = {
-            GET: {},
-            POST: {},
-            PUT: {},
-            DEL: {}
-        };
-    }
+    this.router[req.method][req.url](req, res);
+  }
 
-    setupMethods() {
-        this.get = this.registerRoute('GET');
-        this.post = this.registerRoute('POST');
-        this.put = this.registerRoute('PUT');
-        this.del = this.registerRoute('DEL');
-    }
+  // eslint-disable-next-line class-methods-use-this
+  setupRequest(request) {
+    request.body = '';
 
-    async handleRequest(req, res) {
-        req = await this.setupRequest(req);
-        res = this.setupResponse(res);
+    request.on('data', (chunk) => {
+      request.body += chunk.toString();
+    });
 
-        if (!this.router[req.method][req.url]) {
-            res.statusCode = 404;
-            res.write('not found');
-            return res.end();
-        }
+    // eslint-disable-next-line no-promise-executor-return
+    return new Promise((resolve) => request.on('end', () => {
+      request.body = request.body ? JSON.parse(request.body) : '';
+      resolve(request);
+    }));
+  }
 
-        this.router[req.method][req.url](req, res);
-    }
+  // eslint-disable-next-line class-methods-use-this
+  setupResponse(response) {
+    response.status = (statusCode) => {
+      response.statusCode = statusCode;
+      return response;
+    };
 
-    setupRequest(request) {
-        request.body = '';
+    response.send = (responseBody) => {
+      response.write(responseBody);
+      response.end();
+    };
 
-        request.on('data', chunk => {
-            request.body += chunk.toString();
-        });
+    return response;
+  }
 
-        return new Promise(resolve => request.on('end', () => {
-            request.body = request.body ? JSON.parse(request.body) : '';
-            resolve(request);
-        }));
-    }
+  // eslint-disable-next-line class-methods-use-this
+  registerRoute(method) {
+    return function (route, callback) {
+      this.router[method][route] = callback;
+    };
+  }
 
-    setupResponse(response) {
-        response.status = (statusCode) => {
-            response.statusCode = statusCode;
-            return response;
-        }
-
-        response.send = (responseBody) => {
-            response.write(responseBody);
-            response.end();
-        }
-
-        return response;
-    }
-
-    registerRoute(method) {
-        return function (route, callback) {
-            this.router[method][route] = callback;
-        }
-    }
-
-    listen() {
-        const args = Array.prototype.slice.call(arguments);
-        return this.server.listen.apply(this.server, args);
-    }
+  listen() {
+    const args = Array.prototype.slice.call(arguments);
+    // eslint-disable-next-line prefer-spread
+    return this.server.listen.apply(this.server, args);
+  }
 }
 
 module.exports = new Xpress();
-
