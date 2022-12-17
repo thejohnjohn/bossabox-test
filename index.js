a0a97e7ef895fb8b22ee63786f4a3577bfabe420
thejohnjohn <jp7480566@gmail.com>
2022-12-17 13:28:31 -0300

    refactor: add all methods

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+const querystring = require('querystring');
 const xpress = require('./utils/xpress');
 const { ToolsService } = require('./src/tools/tools.service');
-const { getRequestData } = require('./utils/utils');
 const { isAuthenticated, createToken, verifyToken } = require('./utils/tokenhandler');
 const { HOST, PORT } = require('./env');
 
@@ -8,6 +8,14 @@ const hostname = HOST || '127.0.0.1';
 const port = PORT || 3000;
 
 const toolsService = new ToolsService();
+
+const getToken = (request) => {
+  const headers = request.rawHeaders;
+  const authorizationIndex = headers.indexOf('Authorization') + 1;
+  const token = headers[authorizationIndex].split('Bearer ')[1];
+
+  return token;
+};
 
 xpress.post('/login', async (request, response) => {
   const data = request.body;
@@ -25,26 +33,53 @@ xpress.post('/login', async (request, response) => {
   }
 });
 
-xpress.put('/tools/4', async (request, response) => {
+xpress.post('/tools', async (request, response) => {
+  const data = request.body;
+  const insertedData = await toolsService.insertTool(data);
+
+  response.status(200).send(JSON.stringify(insertedData));
+});
+
+xpress.get('/tools', async (request, response) => {
   const { url } = request;
+  const parameters = querystring.decode(url.split('?')[1]);
+
+  let toolList = '';
+  if (Object.keys(parameters).length === 0) {
+    toolList = await toolsService.getAllTools();
+  } else {
+    if (Array.isArray(parameters.tags) !== true) {
+      parameters.tags = [parameters.tags];
+    }
+    toolList = await toolsService.getToolsByTag([...parameters.tags]);
+  }
+
+  response.writeHead(200, { 'Content-Type': 'application/json' });
+  response.status(200).send(JSON.stringify(toolList));
+});
+
+xpress.put('/tools/4', async (request, response) => {
+  const { url, body } = request;
   const id = parseInt(url.split('/')[2], 10);
 
-  const headers = request.rawHeaders;
-  const authorizationIndex = headers.indexOf('Authorization') + 1;
-  const token = headers[authorizationIndex].split('Bearer ')[1];
+  const token = getToken(request);
   const result = verifyToken(token);
 
   if (result instanceof Error) {
-    response.writeHead(200, { 'Content-Type': 'application/json' });
-    response.end('Access expired.');
+    response.status(401).send('Acess expired');
   }
 
-  const updatedTool = request.body;
-  console.log(updatedTool);
-  /* await toolsService.updateToolById(id, JSON.parse(updatedTool));
+  const updatedTool = await toolsService.updateToolById(id, JSON.parse(body));
 
-  response.writeHead(200, { 'Content-Type': 'application/json' }); */
+  response.writeHead(200, { 'Content-Type': 'application/json' });
   response.status(200).send(JSON.stringify(updatedTool));
+});
+
+xpress.del('/tools/4', async (request, response) => {
+  const idToDelete = parseInt(request.url.split('/')[2], 10);
+  await toolsService.deleteToolById(idToDelete);
+
+  response.status(200).send('OK');
 });
 
 xpress.listen(port, () => console.log(`Server running at http://${hostname}:${port}/`));
